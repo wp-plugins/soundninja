55e58ca7a94f633c08468dfc941cfbd5e204d056
dysonross <dysonross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-18 21:31:21 +0000

    v0.1.8
    git-svn-id: https://plugins.svn.wordpress.org/soundninja/tags/0.1.8@1288968 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,85 @@
+=== Soundninja ===
+Contributors: rossdyson
+Donate link: http://soundninja.com
+Tags: audio, music, discovery, user engagement, advertising
+Requires at least: 3.0.1
+Tested up to: 4.3.1
+Stable tag: 0.1.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The Wordpress plug-in Spotify wish they made.
+
+== Description ==
+
+Soundninja is the easiest way to integrate Spotify song previews on your website. It integrates seamlessly with your content and allows readers to preview tracks without leaving the page.
+
+You can download and use Soundninja on your site for free, however for our automatic artist detection to work properly you need to register through [Soundninja.com](https://www.soundninja.com). Registration is [free](https://www.soundninja.com).
+
+== Installation ==
+
+1. Search for Soundninja under "Admin" → "Plugins" → "Add New" or upload the contents of the Soundninja plugin to the `/wp-content/plugins/` directory in a folder called `Soundninja`
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Register on our website at [www.soundninja.com](https://www.soundninja.com)
+4. Copy and paste your **Site ID** and **Access Token** into the plugin options in Wordpress, under "Settings" → "Soundninja"
+5. Follow our guides to manage and set up Soundninja on your site
+
+== Frequently Asked Questions == 
+
+= Do I need an account to use this? =
+
+Yes, get in touch with team@soundninja.com to request a Client ID and Access Key.
+
+= What Is Soundninja? =
+
+Soundninja is an in-context music discovery tool; we automatically detect artist names within your web page content and give your readers an easy way to preview those artists’ songs without having to leave your page.
+
+= Does Soundninja affect my website’s performance? =
+
+No. Soundninja is very lightweight, the JavaScript is around 90kb in size and is super fast to download on an average connection. Our JavaScript loads asynchronously, which means it doesn’t block the other elements of your page from loading. When Soundninja has downloaded we analyse your page in the background after the content has finished loading.
+
+= Why are some artists not automatically recognized? =
+
+We use advanced text processing algorithms to identify artist names. The technology is still in beta and undergoing improvements, but if an artist is not automatically recognized you can utilize the Soundninja admin panel to manually add the artist.
+
+= Which browsers does Soundninja support? =
+
+We support all popular browsers across both desktop and mobile! :D
+
+== Screenshots ==
+
+== Changelog ==
+
+= 0.1.8 =
+* Enabled sub-categories to be selected inside Soundninja settings
+* Fixed 'James' bug
+
+= 0.1.7 =
+* Modified page selection method to go by categories instead of posts
+* Fixed bug prevent use of Soundninja on mobile devices
+
+= 0.1.6 =
+* Fixed major IE bug
+* Fixed issues with selecting pages correctly
+
+= 0.1.5 =
+* Fixed bug causing Soundninja to load on non-selected pages
+
+= 0.1.4 =
+* Fixed bug in selecting all posts in Soundninja settings
+
+= 0.1.3 =
+* Changed select page list to checkbox list
+* Added search functionality for finding posts
+
+= 0.1.2 =
+* Added a check for Client ID and Access Key
+* Created separate settings page for Soundninja settings
+
+= 0.1.1 =
+* Added support for selecting pages and post to run the Soundninja plug-in on
+* Added support to run Soundninja on homepage
+
+== Upgrade Notice == 
+
+= 0.1.0 =
---
+++ soundninja.php
@@ -0,0 +1,237 @@
+<?php
+/*
+Plugin Name: Soundninja 
+Plugin URI:  http://soundninja.com/
+Description: The Wordpress plug-in Spotify wish they made
+Version:     0.1.8
+Author:      Web Three
+Author URI:  http://soundninja.com/
+Copyright (C) 2015  Web Three Inc.
+*/
+
+$soundninja_plugin_slug = 'soundninja';
+
+add_action('wp_enqueue_scripts', 'soundninja_enqueue');
+// enqueue soundninja javascript files in frontend
+function soundninja_enqueue_init() {
+	wp_enqueue_script('soundninja_settings', // id
+	    'http://soundninja.github.io/demos/build/settings/' . get_option('soundninja_client_id') . '.js', // path
+	    array('jquery','soundninja'), // dependencies
+	    0, // appends ?ver=$wordpress_version
+	    true // in_footer
+	);
+
+	wp_enqueue_script('soundninja', // id
+	    'http://soundninja-274b.kxcdn.com/live/Soundninja.min.js', // path
+	    array('jquery'), // dependencies
+	    0, // appends ?ver=$wordpress_version
+	    true // in_footer
+	);
+    
+    add_filter( 'script_loader_tag', function ( $tag, $handle ) {
+    if ( 'soundninja' !== $handle )
+        return $tag;
+    return str_replace( ' src', ' id="soundninja" src', $tag );
+    }, 10, 2 );
+}
+
+function soundninja_enqueue($hook) {
+    $type = get_post_type();
+
+    // checking whether page or post
+    //
+    // Sergey aka Takereal. 2015-10-20 update. By categories.
+    //
+    if (is_singular('post') && $type=='post')  {
+
+      $option = get_option('soundninja_show_on_categories');
+
+      if(!empty($option)) {
+
+        foreach(wp_get_post_categories(get_the_ID()) as $category_id) {
+
+          if(in_array($category_id, $option)) {
+
+            soundninja_enqueue_init();
+            break;
+          }
+        }
+      }
+
+    }
+    elseif(is_page()) {
+    	$option = get_option('soundninja_show_on_pages');
+      	if(!empty($option) && in_array(get_the_ID(),$option) || !empty($option) && in_array('all',$option)) {
+        	soundninja_enqueue_init();
+      	}
+    }
+    // checking whether home or front page
+ 	elseif(get_option('soundninja_show_on_home') && (is_home() || is_front_page())){
+    	soundninja_enqueue_init();
+  	}
+}
+
+
+// adding sub menu "Soundninja" under Settings menu
+add_action('admin_menu', 'admin_ninja_menu');
+function admin_ninja_menu() {
+	global $soundninja_plugin_slug;
+	add_submenu_page('options-general.php', 'Soundninja Settings', 'Soundninja', 'manage_options', $soundninja_plugin_slug, 'soundninja_settings_page');
+}
+
+// Soundninja Settings page
+function soundninja_settings_page() {
+?>
+    <!-- select all options of dropdown -->
+    <script type="text/javascript">
+        function dropdown_change(obj) {
+            var isAllSelected = jQuery(obj).find("option:first-child").prop('selected');
+            if (isAllSelected)
+                jQuery(obj).find("option:not(:first-child)").prop('selected', false);
+            else
+                jQuery(obj).find("option:first-child").prop('selected', false);
+        }
+    </script>
+    <div class="wrap">
+        <h2>Soundninja Settings</h2>
+        <form method="post" action="options.php" id="soundninja_options">
+            <?php settings_fields( 'soundninja-settings-group' ); ?>
+                <?php do_settings_sections( 'soundninja-settings-group' ); ?>
+
+                    <table class="form-table">
+                        <tr valign="top">
+                            <th scope="row">
+                                <?php _e( 'Site ID', 'soundninja-plugin' ) ?>:</th>
+                            <td>
+                                <input type="text" name="soundninja_client_id" style="width:626px;padding:2px 10px;" 
+					value="<?php echo esc_attr( get_option('soundninja_client_id') ); ?>">
+                                    
+                                
+                            </td>
+                        </tr>
+
+                        <tr valign="top">
+                            <th scope="row">
+                                <?php _e( 'Access Token', 'soundninja-plugin' ) ?>:</th>
+                            <td>
+				
+                                <input type="text"name="soundninja_access_token" style="width:626px; padding:2px 10px;"
+					value="<?php echo esc_attr( get_option('soundninja_access_token') ); ?>">
+                                    
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <?php
+
+//
+// Sergey aka Takereal update begins here.
+//
+?>
+                                <th scope="row">
+                                    <?php _e( 'Categories', 'soundninja-plugin' ) ?>:</th>
+                                <td>
+                                    <?php _e( '<i>Choose which post categories to use Soundninja on:</i>' ); ?>
+                                        <br/>
+                                        <ul style="list-style: none;">
+                                            <?php $selected = get_option('soundninja_show_on_categories','no'); ?>
+                                                <?php
+						$args = array(
+	'type'                     => 'post',
+	'child_of'                 => 0,
+	'parent'                   => '',
+	'orderby'                  => 'term_group',
+	'order'                    => 'ASC',
+	'hide_empty'               => 0,
+	'hierarchical'             => 1,
+	'exclude'                  => '',
+	'include'                  => '',
+	'number'                   => '',
+	'taxonomy'                 => 'category',
+	'pad_counts'               => false );
+						$categories = get_categories($args);
+						foreach ($categories as $category) {
+							// loading all categories and showing in dropdown
+							$id = $category->cat_ID;
+							$selected_option  = (is_array($selected) && in_array($id,$selected)) ? ' checked="checked" ' : '';
+						?>
+		           <li style="width: auto; float: left; margin-right: 15px;">
+                                                        <input class="ncheckbox" type="checkbox" name="soundninja_show_on_categories[]" value="<?php echo $id; ?>" ctitle="<?php echo $category->category_nicename; ?>" <?php echo $selected_option; ?> />
+                                                        <?php echo $category->category_nicename; ?>
+                                                    </li>
+
+                                                    <?php } ?>
+                                        </ul>
+                                </td>
+                        </tr>
+
+                        <tr valign="top">
+                            <th scope="row">
+                                <?php _e( 'Pages', 'soundninja-plugin' ) ?>:</th>
+                            <td>
+                                <?php _e( '<i>Choose which pages to use Soundninja on:</i>' ); ?>
+                                    <br/>
+                                    <ul style="list-style: none;">
+                                        <?php $selected = get_option('soundninja_show_on_pages','a'); ?>
+                                            <?php
+							// loading all pages and showing in dropdown
+							query_posts( 'posts_per_page=-1&post_type=page&orderby=title&order=ASC' );
+							while ( have_posts() ) : the_post();
+								$id =  get_the_ID();
+								$selected_option  = (is_array($selected) && in_array($id,$selected)) ? ' checked="checked" ' : '';
+						?>
+                                                <li style="width: auto; float: left; margin-right: 15px;">
+                                                    <input class="ncheckboxpage" type="checkbox" name="soundninja_show_on_pages[]" value="<?php echo $id ?>" ctitle="<?php the_title(); ?>" <?php echo $selected_option; ?> />
+                                                    <?php the_title(); ?>
+                                                </li>
+                                                <?php endwhile; ?>
+
+                                                    <?php wp_reset_query(); ?>
+                                    </ul>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <?php _e( 'Show on home page', 'soundninja-plugin' ) ?>:</th>
+                            <td>
+
+                                <input type="checkbox" id="show_home" name="soundninja_show_on_home" value="1" <?php echo checked(1, get_option( 'soundninja_show_on_home'), false); ?> />
+                                <label for="show_home">
+                                    <?php _e('Run Soundninja on Homepage', 'soundninja-plugin' ); ?>
+                                </label>
+                            </td>
+                        </tr>
+                    </table>
+                    <p class="submit">
+                        <input type="submit" value="Save Changes" class="button button-primary" id="submit" name="submit">
+                    </p>
+        </form>
+    </div>
+
+    <?php
+}
+
+
+add_action('admin_init', 'register_ninja');
+function register_ninja() {
+	// registering settings
+
+	register_setting('soundninja-settings-group', 'soundninja_client_id');
+	register_setting('soundninja-settings-group', 'soundninja_access_token');
+register_setting('soundninja-settings-group', 'soundninja_show_on_categories');	// Sergey aka Takereal 2015-10-20
+    register_setting('soundninja-settings-group', 'soundninja_show_on_pages');
+  	register_setting('soundninja-settings-group', 'soundninja_show_on_home');
+}
+
+//Add the filter with your plugin information
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'soundninja_plugin_action_links' );
+
+//The callback function to add the settings link
+function soundninja_plugin_action_links( $links ) {
+	global $soundninja_plugin_slug;
+	return array_merge(
+    	array(
+      	'settings' => '<a href="' . admin_url( 'options-general.php?page=' . $soundninja_plugin_slug ) . '">' . __( 'Settings', 'soundninja-plugin' ) . '</a>'
+    	),
+    	$links
+  	);
+}
